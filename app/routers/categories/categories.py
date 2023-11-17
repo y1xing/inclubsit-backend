c335af94d9989d76ab118038ceb8d9eef78148b8
y1xing <80627162+y1xing@users.noreply.github.com>
2023-11-17 14:55:04 +0800

    Categories endpoint edited and fixed

--- app/routers/categories/categories.py
+++ app/routers/categories/categories.py
@@ -28,6 +28,8 @@ router = APIRouter(
     tags=['categories']
 )
 
+CATEGORY_IMAGE_PATH = "CategoryImages"
+CLUBS_IMAGE_PATH = "ClubImages"
 
 auth_responses = {
     200: {"description": "OK"},
@@ -54,13 +56,20 @@ async def all(response: Response):
     """
     try:
         result = sql_adapter.query(
-            "SELECT cc.ClubCategoryName, COUNT(*) FROM Club c LEFT JOIN ClubCategory cc on c.ClubCategoryID = cc.ClubCategoryID GROUP BY cc.ClubCategoryID")
+            "SELECT cc.ClubCategoryID, cc.ClubCategoryName, COUNT(*) FROM Club c LEFT JOIN ClubCategory cc on c.ClubCategoryID = cc.ClubCategoryID GROUP BY cc.ClubCategoryID")
 
-        result_formatted = [{"name": category[0],
-                             "clubs": category[1]} for category in result]
+        images = firebase_adapter.get_category_images()
+
+        result_formatted = [
+            {
+                "id": category[0],
+                "title": category[1],
+                "media": images[str(category[0])]['image'],
+                "numberOfClubs": category[1]
+            } for category in result]
 
     except Exception as e:
-        print(e)
+        print("Error is", e)
         return {"message": "Error in fetching categories"}, 400
 
     return {"message": "All categories data fetched", "data": result_formatted}, 200
@@ -73,23 +82,43 @@ async def get_category(category_id: str, response: Response):
     """
 
     try:
+        print("Category_id", category_id)
         clubs = sql_adapter.query(
             "SELECT c.ClubID, c.ClubName, COUNT(*), c.ClubTrainingDates, c.ClubTrainingLocations FROM Club c LEFT JOIN ClubMember cm on c.ClubID = cm.ClubID WHERE c.ClubCategoryID = %s GROUP BY cm.ClubID", (category_id, ))
         category_info = sql_adapter.query(
-            "SELECT c.ClubCategoryName, cc.CategoryDescription FROM ClubCategoryInformation cc, ClubCategory c WHERE c.ClubCategoryID = %s", (category_id, ))[0]
+            "SELECT c.ClubCategoryName, cc.CategoryDescription FROM ClubCategory c LEFT JOIN ClubCategoryInformation cc on c.ClubCategoryID = cc.ClubCategoryID WHERE cc.ClubCategoryID = %s", (category_id, ))[0]
+
+        print('category_info', category_info)
+
+        images = firebase_adapter.get_club_images()
+
+        def members_estimate(members):
+            if members <= 10:
+                return "1-10"
+            elif members > 10 and members <= 20:
+                return "11-20"
+            elif members > 20 and members <= 30:
+                return "21-30"
+            elif members > 30 and members <= 40:
+                return "31-40"
+            elif members > 40 and members <= 50:
+                return "41-50"
+            else:
+                return "50+"
 
         result_formmated = {
             "category_info": {
-                "name": category_info[0],
+                "title": category_info[0],
                 "description": category_info[1],
             },
             "clubs": [
                 {
                     "id": club[0],
                     "title": club[1],
-                    "members": club[2],
+                    "members": members_estimate(club[2]),
                     "training": club[3],
                     "location": club[4],
+                    "media": images[str(club[0])]['logo']
                 } for club in clubs]
         }
 
@@ -97,4 +126,4 @@ async def get_category(category_id: str, response: Response):
         print(e)
         return {"message": "Error in fetching category"}, 400
 
-    return {"message": "Category data fetched successfully", "data": result_formmated}
+    return {"message": "Category data fetched successfully", "data": result_formmated}, 200
